5a3c16593b2326580d763b06ce8949f237e3b7fa
levchenkokirill040700 <158365352+levchenkokirill040700@users.noreply.github.com>
2024-08-02 14:39:18 +0300

    Remove unused code

---
+++ InsertChampionship.cs
@@ -0,0 +1,26 @@
+﻿using ICD.Base.Domain.Entity;
+using ICD.Framework.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICD.Base
+{
+    public class InsertChampionshipRequest: Request<InsertChampionshipResult>
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class InsertChampionshipResult: SingleQueryResult<InsertChampionshipModel> { }
+
+    public class InsertChampionshipModel
+    {
+
+    }
+}
---
+++ UpdateChampionship.cs
@@ -0,0 +1,25 @@
+﻿using ICD.Framework.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICD.Base
+{
+    public class UpdateChampionshipRequest : Request<UpdateChampionshipResult>
+    {
+        public int Key { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+    }
+    public class UpdateChampionshipResult:SingleQueryResult<UpdateUpdateChampionshipModel>{ }
+
+    public class UpdateUpdateChampionshipModel
+    {
+
+    }
+}
