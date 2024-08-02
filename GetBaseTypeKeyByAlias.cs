194d42f54634cdcda69c0331488ee96f7b348114
levchenkokirill040700 <158365352+levchenkokirill040700@users.noreply.github.com>
2024-08-02 14:38:59 +0300

    enhance_security

---
+++ GetBaseTypeKeyByAlias.cs
@@ -0,0 +1,14 @@
+﻿using ICD.Framework.Model;
+
+namespace ICD.Base
+{
+    public class GetBaseTypeKeyByAliasQuery : Query
+    {
+        public string Alias { get; set; }
+    }
+    public class GetBaseTypeKeyByAliasResult : SingleQueryResult<GetBaseTypeKeyByAliasModel> { }
+    public class GetBaseTypeKeyByAliasModel
+    {
+        public int BaseTypeKey { get; set; }
+    }
+}
